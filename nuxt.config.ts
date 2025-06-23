77fd31ade7de45625073b4b11024add850cb3d3e
mvillarinos <manuvillarinos@gmail.com>
2025-06-23 01:30:57 -0300

    #DEV Update nuxt config + favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  site: {
+    url: 'https://localhost:3000',
+    name: 'Hoy Bailaré',
+    currentLocale: 'es_AR'
+  },
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  appDir: 'app',
+  components: [{
+    path: '~/components',
+    pathPrefix: false
+  }],
   modules: [
     '@nuxt/content',
     '@nuxt/icon',
@@ -12,5 +20,28 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@vueuse/nuxt',
     '@nuxtjs/seo'
-  ]
+  ],
+  seo: {
+    meta: {
+      description: 'My awesome website',
+      themeColor: [
+        { content: '#18181b', media: '(prefers-color-scheme: dark)' },
+        { content: 'white', media: '(prefers-color-scheme: light)' },
+      ],
+      author: 'Manuel Villarinos',
+      colorScheme: 'light',
+      applicationName: 'hoy-bailare-app',
+
+      // Nuxt SEO Utils already sets the below tags for you
+      ogSiteName: 'Hoy Bailaré',
+      ogLocale: 'es_AR',
+      ogType: 'website',
+      ogUrl: 'https://localhost:3000/',
+      ogTitle: 'Hoy Bailaré',
+
+      // Other Nuxt SEO modules handles these
+      ogImage: 'https://example.com/my-og-image.png',
+      robots: 'index, follow',
+    }
+  }
 })
